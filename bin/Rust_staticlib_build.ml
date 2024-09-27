7fe25cd3fc30aa317e63b988560eae9d804d9ece
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-09-27 19:42:49 +0400

    Properly print diagnostic messages from json lines

--- bin/Rust_staticlib_build.ml
+++ bin/Rust_staticlib_build.ml
@@ -1,30 +1,5 @@
-open Unix
 open Yojson.Safe
 open Yojson.Safe.Util
-
-let run_cargo_build crate_name =
-  let command =
-    Printf.sprintf
-      "cargo build --release --offline --package %s --message-format json"
-      crate_name
-  in
-  Printf.printf "Running cargo build command: %s\n%!" command;
-  let input = open_process_in command in
-  let rec read_lines acc =
-    try
-      let line = input_line input in
-      read_lines (line :: acc)
-    with
-    | End_of_file -> List.rev acc
-    | e -> failwith ("Error reading cargo output: " ^ Printexc.to_string e)
-  in
-  let lines = read_lines [] in
-  match close_process_in input with
-  | WEXITED 0 -> lines
-  | WEXITED code -> failwith ("Cargo build failed with exit code " ^ string_of_int code)
-  | WSIGNALED signal -> failwith ("Cargo build killed by signal " ^ string_of_int signal)
-  | WSTOPPED signal -> failwith ("Cargo build stopped by signal " ^ string_of_int signal)
-;;
 
 let parse_json_line line =
   try Some (from_string line) with
@@ -36,6 +11,48 @@ let is_compiler_artifact json =
   match json |> member "reason" |> to_string_option with
   | Some "compiler-artifact" -> true
   | _ -> false
+;;
+
+let is_diagnostic_message json =
+  match json |> member "reason" |> to_string_option with
+  | Some "compiler-message" -> true
+  | _ -> false
+;;
+
+let run_cargo_build crate_name =
+  let command =
+    Printf.sprintf
+      "cargo build --release --offline --package %s --message-format json"
+      crate_name
+  in
+  Printf.printf "Running cargo build command: %s\n%!" command;
+  let input = Unix.open_process_in command in
+  let rec read_lines acc =
+    try
+      let line = input_line input in
+      let acc =
+        match parse_json_line line with
+        | Some json when is_diagnostic_message json ->
+          Printf.fprintf
+            stderr
+            "Compiler message: %s\n%!"
+            (json |> member "message" |> member "message" |> to_string);
+          line :: acc
+        | _ -> line :: acc
+      in
+      read_lines acc
+    with
+    | End_of_file -> List.rev acc
+    | e ->
+      Printf.fprintf stderr "Error reading cargo output: %s\n%!" (Printexc.to_string e);
+      exit 1
+  in
+  let lines = read_lines [] in
+  match Unix.close_process_in input with
+  | WEXITED 0 -> lines
+  | WEXITED code -> failwith ("Cargo build failed with exit code " ^ string_of_int code)
+  | WSIGNALED signal -> failwith ("Cargo build killed by signal " ^ string_of_int signal)
+  | WSTOPPED signal -> failwith ("Cargo build stopped by signal " ^ string_of_int signal)
 ;;
 
 let get_target_name json =
@@ -53,32 +70,32 @@ let copy_file src dst =
   let buf = Bytes.create buf_size in
   (* Error handling for file open *)
   let fd_in =
-    try openfile src [ O_RDONLY ] 0 with
-    | Unix_error (err, _, _) ->
-      failwith ("Error opening source file: " ^ error_message err)
+    try Unix.openfile src [ O_RDONLY ] 0 with
+    | Unix.Unix_error (err, _, _) ->
+      failwith ("Error opening source file: " ^ Unix.error_message err)
   in
   let fd_out =
-    try openfile dst [ O_WRONLY; O_CREAT; O_TRUNC ] 0o644 with
-    | Unix_error (err, _, _) ->
-      close fd_in;
-      failwith ("Error opening destination file: " ^ error_message err)
+    try Unix.openfile dst [ O_WRONLY; O_CREAT; O_TRUNC ] 0o644 with
+    | Unix.Unix_error (err, _, _) ->
+      Unix.close fd_in;
+      failwith ("Error opening destination file: " ^ Unix.error_message err)
   in
   let rec copy_loop () =
-    match read fd_in buf 0 buf_size with
+    match Unix.read fd_in buf 0 buf_size with
     | 0 -> ()
     | n ->
-      if write fd_out buf 0 n <> n then failwith "Error writing to destination file";
+      if Unix.write fd_out buf 0 n <> n then failwith "Error writing to destination file";
       copy_loop ()
   in
   try
     copy_loop ();
-    close fd_in;
-    close fd_out
+    Unix.close fd_in;
+    Unix.close fd_out
   with
-  | Unix_error (err, _, _) ->
-    close fd_in;
-    close fd_out;
-    failwith ("Error copying file: " ^ error_message err)
+  | Unix.Unix_error (err, _, _) ->
+    Unix.close fd_in;
+    Unix.close fd_out;
+    failwith ("Error copying file: " ^ Unix.error_message err)
 ;;
 
 (* Function to replace '-' with '_' in crate name *)
