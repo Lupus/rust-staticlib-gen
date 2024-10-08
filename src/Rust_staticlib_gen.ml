4c86016f1fafb31cefe0ed88cac44b18039c12f2
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-10-08 10:07:15 +0400

    Remove unused code

--- src/Rust_staticlib_gen.ml
+++ src/Rust_staticlib_gen.ml
@@ -42,34 +42,6 @@ let generate_command params =
       Rust_staticlib.gen_staticlib st params f opam))
 ;;
 
-let output_filename =
-  let doc = "Output filename for the generated dune file" in
-  Arg.(value & opt string "dune.inc.gen" & info [ "o"; "output" ] ~docv:"OUTPUT" ~doc)
-;;
-
-let local_crate_path =
-  let doc =
-    "Path (relative) to local crate which contains stubs for specified opam file"
-  in
-  Arg.(
-    value
-    & opt (some string) None
-    & info [ "l"; "local-crate-path" ] ~docv:"LOCAL_CRATE_PATH" ~doc)
-;;
-
-let opam_file =
-  let doc = "Opam file to process" in
-  Arg.(required & pos 0 (some file) None & info [] ~docv:"OPAM_FILE" ~doc)
-;;
-
-let extra_crate_paths =
-  let doc = "Additional paths (relative) to extra crates to be included in Cargo.toml" in
-  Arg.(
-    value
-    & opt_all string []
-    & info [ "e"; "extra-crate-path" ] ~docv:"EXTRA_CRATE_PATH" ~doc)
-;;
-
 let main params =
   Random.self_init ();
   OpamSystem.init ();
